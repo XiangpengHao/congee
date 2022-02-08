953f524a19577797893cb370b14c0e9a8482131d
Xiangpeng Hao <haoxiangpeng123@gmail.com>
2022-02-08 16:02:06 -0500

    fix mem leak

--- src/node_16.rs
+++ src/node_16.rs
@@ -25,6 +25,42 @@ impl Node16 {
         std::intrinsics::cttz(val)
     }
 
+    fn get_insert_pos(&self, key: u8) -> usize {
+        let flipped = Self::flip_sign(key);
+
+        #[cfg(all(target_feature = "sse2", not(miri)))]
+        {
+            unsafe {
+                use std::arch::x86_64::{
+                    __m128i, _mm_cmplt_epi8, _mm_loadu_si128, _mm_movemask_epi8, _mm_set1_epi8,
+                };
+                let cmp = _mm_cmplt_epi8(
+                    _mm_set1_epi8(flipped as i8),
+                    _mm_loadu_si128(&self.keys as *const [u8; 16] as *const __m128i),
+                );
+                let bit_field = _mm_movemask_epi8(cmp) & (0xFFFF >> (16 - self.base.count));
+                let pos = if bit_field > 0 {
+                    Self::ctz(bit_field as u16)
+                } else {
+                    self.base.count as u16
+                };
+                pos as usize
+            }
+        }
+
+        #[cfg(any(not(target_feature = "sse2"), miri))]
+        {
+            let mut pos = 0;
+            while pos < self.base.count {
+                if self.keys[pos as usize] >= flipped {
+                    return pos as usize;
+                }
+                pos += 1;
+            }
+            pos as usize
+        }
+    }
+
     fn get_child_pos(&self, key: u8) -> Option<usize> {
         #[cfg(all(target_feature = "sse2", not(miri)))]
         unsafe {
@@ -151,10 +187,25 @@ impl Node for Node16 {
         self.base.count == 3
     }
 
+    // Insert must keep keys sorted, is this necessary?
     fn insert(&mut self, key: u8, node: NodePtr) {
         let key_flipped = Self::flip_sign(key);
 
-        let pos = self.base.count as usize;
+        let pos = self.get_insert_pos(key);
+
+        unsafe {
+            std::ptr::copy(
+                self.keys.as_ptr().add(pos),
+                self.keys.as_mut_ptr().add(pos + 1),
+                self.base.count as usize - pos,
+            );
+
+            std::ptr::copy(
+                self.children.as_ptr().add(pos),
+                self.children.as_mut_ptr().add(pos + 1),
+                self.base.count as usize - pos,
+            );
+        }
 
         self.keys[pos] = key_flipped;
         self.children[pos] = node;
