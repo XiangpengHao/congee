3f2ca000df88a93e08d86084ff5ccbb3b58b3fd2
github-action-benchmark <github@users.noreply.github.com>
2022-07-06 05:28:19 +0000

    add Congee basic op performance (customBiggerIsBetter) benchmark result for d97054a05b7bbe33e9bd2a141187129646c3055f

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1657085299096,
+  "repoUrl": "https://github.com/XiangpengHao/congee",
+  "entries": {
+    "Congee basic op performance": [
+      {
+        "commit": {
+          "author": {
+            "email": "haoxiangpeng123@gmail.com",
+            "name": "Xiangpeng Hao",
+            "username": "XiangpengHao"
+          },
+          "committer": {
+            "email": "haoxiangpeng123@gmail.com",
+            "name": "Xiangpeng Hao",
+            "username": "XiangpengHao"
+          },
+          "distinct": true,
+          "id": "d97054a05b7bbe33e9bd2a141187129646c3055f",
+          "message": "try continous benchmark",
+          "timestamp": "2022-07-05T22:23:06-07:00",
+          "tree_id": "7af4b2dd0d40c0375cc59b2edb2a62f26cf44aaf",
+          "url": "https://github.com/XiangpengHao/congee/commit/d97054a05b7bbe33e9bd2a141187129646c3055f"
+        },
+        "date": 1657085298304,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ReadOnly",
+            "value": 10651373,
+            "unit": "QPS"
+          },
+          {
+            "name": "InsertOnly",
+            "value": 3654808,
+            "unit": "QPS"
+          },
+          {
+            "name": "UpdateOnly",
+            "value": 7972816,
+            "unit": "QPS"
+          }
+        ]
+      }
+    ]
+  }
+}
